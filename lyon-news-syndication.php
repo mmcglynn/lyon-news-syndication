6c7af67b7d61c24f2d50bbf47e5d4a42b3964ae6
Michael McGlynn <info@reignition.net>
2016-07-20 20:24:31 -0400

    Initial commit.

---
+++ lyon-news-syndication.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Lyon News Syndication
+Plugin URI:  http://github.com
+Description: Pulls configured content into a widget.
+Version:     .1
+Author:      Michael McGlynn
+Author URI:  http://reignitioninc.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: lyon-news-syndication
+*/
